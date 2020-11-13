c53215325819c0055860164e02b5c8041889a771
Tapia Sontag <s.tapia-sontag@wolterskluwer.com>
2020-11-13 15:38:56 +0100

    Initial commit

---
+++ src/BTCTracker.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import $ from 'jquery';
+
+export default class BTCTracker extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            price: 0.00,
+            lastFetch: ""
+        }
+    }
+
+    componentDidMount() {
+        this.fetch();
+    }
+
+    fetch() {
+        let context = this;
+
+        window.setTimeout(function() {
+            $.ajax({
+                url: "https://api.coindesk.com/v1/bpi/currentprice.json",
+                dataType: "json",
+                method: "GET",
+                success: function(response) {
+                    context.setState({
+                        price: response.bpi.USD.rate,
+                        lastFetch: response.time.updated
+                    });
+                }
+            });
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    BTC Price: {this.state.price}
+                    <small>
+                        {this.state.lastFetch}
+                    </small>
+                </h1>
+            </div>
+        );
+    }
+}
